0f8223a82de794f482283d3257cf9461ef7f52d7
pahud <hunhsieh@amazon.com>
2023-12-15 22:06:32 +0000

    add Queue

--- lib/q-demo-repo-stack.ts
+++ lib/q-demo-repo-stack.ts
@@ -1,10 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class QDemoRepoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new sqs.Queue(this, 'Queue');
 
     // The code that defines your stack goes here
 
