774cd52319ace4cc015c23802884097037f3bc3f
Pahud Hsieh <pahudnet@gmail.com>
2023-12-15 16:26:01 -0500

    Update q-demo-repo-stack.ts

--- lib/q-demo-repo-stack.ts
+++ lib/q-demo-repo-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-
 export class QDemoRepoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
